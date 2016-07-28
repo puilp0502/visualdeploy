582967ca54c0ecd689bba46f2868f32e4a238097
Sunrin.io Maintainer <admin@sunrin.io>
2016-07-28 17:31:39 +0900

    Fix bugs

--- visualdeploy/__init__.py
+++ visualdeploy/__init__.py
@@ -1,17 +1,19 @@
 import os
 import subprocess
 
-def execute(commands, head=None, post_run=None, error_color='00e676', success_color='e57373', hide_homedir=True):
+def execute(commands, head=None, post_run=None, success_color='00e676', error_color='e57373', hide_homedir=True):
     if head is None:
         head = """
     <head>
         <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <title>DynDeploy</title>
+        <title>VisualDeploy</title>
+        <style>body{font-family: sans-serif;}</style>
     </head>"""
     if post_run is None:    
-        post_run = """
+        post_run = """<script>
     window.scroll(0, document.body.scrollHeight);
-    $("#output-%s").css({"background-color": "#%s"});
+    $("#output-{i}").css({"background-color": "#{color}"});
+</script>
     """
 
     home = os.getenv('HOME')
@@ -24,7 +26,8 @@ def execute(commands, head=None, post_run=None, error_color='00e676', success_co
 
     yield '<html>'+head+'<body><h1>DynamicDeploy</h1>'
     
-    for i, cmd in enumerate(commands):
+    i = 1
+    for cmd in commands:
         is_secret = False
         cmd = cmd.replace('~', home) # '~' shortcut doesn't work with os-like funcions
         
@@ -36,7 +39,8 @@ def execute(commands, head=None, post_run=None, error_color='00e676', success_co
 
         if cmd[0:2] == "cd":  # cd doesn't work; we need to use chdir
             os.chdir(cmd[2:].strip())
-            if not is_secret: yield '<div>Changing directory to: '+getcwd()+'</div>'
+            if not is_secret: yield '<div>Changing directory to: '+getcwd()+'</div></div><hr/>'
+            i += 1
             continue
 
         if not is_secret:
@@ -44,13 +48,18 @@ def execute(commands, head=None, post_run=None, error_color='00e676', success_co
         print(cmd)
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
-        if is_secret:  # Secret commands don't need I/O handling
+        if is_secret:  # Secret commands don't need any further processing
             proc.wait()
             continue
 
         for line in proc.stdout:
-            line = line.decode('utf-8').replace('\n', '<br>').encode('utf-8')
+            line = line.decode('utf-8', errors='ignore').replace('\n', '<br>').encode('utf-8')
             yield line
         proc.stdout.close()
         ret_code = proc.wait()
-
+        color = success_color if ret_code == 0 else error_color
+        yield "</div><h4>Process returned: "+str(ret_code)+"</h4>"
+        yield post_run.replace('{i}', str(i)).replace('{color}', str(color))
+        yield "</div><hr/>"
+        i += 1
+    yield "<h3>Deploy Completed!</h3>"
